36d3aa383bb684b1ddc1bbbc03ebe4853da670c7
Andrey <progandrey@gmail.com>
2019-12-07 23:04:45 +0200

    add Module.php

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace itshkacomua\notepade;
+
+/**
+ * notepade module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'itshkacomua\notepade\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
